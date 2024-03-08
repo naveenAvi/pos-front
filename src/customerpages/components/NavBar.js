96ce662226970c1f5af74245677a9c23861408e7
Naveen Avishka <naveen1234avishka@gmail.com>
2024-03-08 22:26:08 +0530

    finale

--- src/customerpages/components/NavBar.js
+++ src/customerpages/components/NavBar.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function NavBar() {
 
@@ -35,11 +35,9 @@ export default function NavBar() {
                         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                             <a class="navbar-brand" href="">POS Software</a>
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                
                                 <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="" onClick={home}>Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href='' >Cart</a>
+                                    <Link class="nav-link" to='/' >Cart</Link>
                                 </li>
                             </ul>
                             <form class="d-flex" role="search">
