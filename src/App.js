d66c25863c0f9a4df37c9b6a290d8a0582cd5b18
Naveen Avishka <naveen1234avishka@gmail.com>
2024-03-08 22:28:21 +0530

    finale

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminHome from './adminpages/AdminHome';
-import AdminItem from './adminpages/AdminItem';
-import AdminStock from './adminpages/AdminStock';
 import AdminCustomer from './adminpages/AdminCustomer';
 import AdminOrder from './adminpages/AdminOrder';
 import Home from './customerpages/Home';
@@ -13,7 +11,6 @@ import AdminStocks from './adminpages/Stocks/AdminStocks';
 import AdminItems from './adminpages/Items/AdminItems';
 import LoginN from './customerpages/Auth/LoginN';
 import RegisterN from './customerpages/Auth/Registern';
-//import AdminItems from './adminpages/Stocks/AdminItems';
 
 function App() {
   return (
@@ -22,32 +19,18 @@ function App() {
         <Routes>
 
 
-          {/* <Route element={<AdminProtectedRoutes />}> */}
             <Route path='/admin' element={<AdminHome />} />
             <Route path='/admin/categories' element={<AdminCategory />} />
             <Route path='/admin/items' element={<AdminItems />} />
             <Route path='/admin/stocks' element={<AdminStocks />} />
-            {/* <Route path='/admin/new/stocks' element={<AdminStocks />} /> */}
             <Route path='/admin/orders' element={<AdminOrder />} />
             <Route path='/admin/customers' element={<AdminCustomer />} />
 
-            <Route path='/admin/stock/old' element={<AdminStock />} />
-
-
-            
-            {/* <Route path='/admin/new/items' element={<AdminItems />} /> */}
-          {/* </Route> */}
-          
             <Route path='/' element={<Home />} />
        
-
-          {/* <Route path='/login' element={<Login />} /> */}
           <Route path='/login' element={<LoginN />} />
           <Route path='/register' element={<RegisterN />} />
-          
-
-          
-          {/* <Route path='/register' element={<Register />} /> */}
+        
         </Routes>
       </BrowserRouter>
     </div>
